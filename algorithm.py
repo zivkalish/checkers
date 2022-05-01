db7d9f6e696ff8f08c883c60fbc08c3567523190
ziv kalish <zack.kalish@gmail.com>
2022-05-01 18:51:49 +0300

    adding pruning WIP

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,11 @@ from copy import deepcopy
 import pygame
 from consts import BLACK, WHITE
 
-def minimax(board, depth, max_player, game):
+def minimax(board, depth, max_player, game, alpha=float('-inf'), beta=float('inf')):
+	"""
+	:alpha: best already explored evaluation for maximizer
+	:beta: best already explored evaluation for mininizer
+	"""
 	if depth == 0 or board.winner(game.turn):
 		return board.evaluate(game.turn), board
 	if max_player:
@@ -15,11 +19,17 @@ def minimax(board, depth, max_player, game):
 		max_func = min
 	best_board = None
 	for simulated_board in get_all_boards(board, color):
-		evaluation = minimax(simulated_board, depth-1, not(max_player), game)[0]
+		evaluation = minimax(simulated_board, depth-1, not(max_player), game, alpha=beta, beta=alpha)[0]
 		max_eval = max_func(max_eval, evaluation)
 		if max_eval == evaluation:
 			best_board = simulated_board
+		# pruning
+		if alpha == max_func(alpha, max_eval):
+			alpha = max_eval
+		if alpha >= beta:
+			break
 	return max_eval, best_board
+
 
 
 def get_all_boards(board, color):
