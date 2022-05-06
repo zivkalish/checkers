7f71da23d738be6d1c8a09386886d641aed29c60
ziv kalish <zack.kalish@gmail.com>
2022-05-06 16:19:11 +0300

    bugfix

--- algorithm.py
+++ algorithm.py
@@ -18,7 +18,7 @@ def minimax(board, depth, turn, opponent_best_score=None):
 		should_prune = lambda score, opponent_best_explored_score: score <= opponent_best_explored_score
 	best_board = None
 	for simulated_board in get_all_boards(board, turn):
-		score = minimax(board, depth-1, next_turn, best_score)[0]
+		score = minimax(simulated_board, depth-1, next_turn, best_score)[0]
 		best_score = optimizer_func(score, best_score)
 		if best_score == score:
 			best_board = simulated_board
